1698569e1ecafcc9f4b7c396bf327a4e138ca52a
Jocelyn Rodriguez <jrodriguez9205@hotmail.com>
2018-05-28 17:25:20 -0400

    cambios en html, ccs y js

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
+//LO QUE SE VEN EN LA CONSOLA
+window.onload = function (){
+
 function add(numUno,numDos){ //PRIMERA FUNCION
     return numUno+numDos; //SUMA
-}
+ }
 
 function multiply(numUno,numDos){ //SEGUNDA FUNCION
     return numUno*numDos; //MULTIPLICACION
-}
+ }
 
 var suma = add(9824,777); //VARIBLE QUE REALIZA LA SUMA
 var final = multiply(suma,36325); //VARIABLE QUE REALIZA SUMA Y MULTIPLICACION
 console.log(final); //RESULTADO FINAL QUE VA A MOSTRAR LA CONSOLA
 
-//SI COPIO TODO EL LA CONSOLA EL RESULTADO QUE DEBERIA APARECER ES = 385081325
+} //SI COPIO TODO EL LA CONSOLA EL RESULTADO QUE DEBERIA APARECER ES = 385081325
+
+
+//LO QUE SE VE EN LA PAGINA WEB
+var btn = document.getElementById("button-brb"); //BOTON
+var txt = document.getElementById ("final"); //BOTON
+btn.addEventListener('click', function(){ //FUNCION AL HACER CLICK
+    txt.innerHTML = "<p> El resultado final es 385.081.325 </p>"; //TEXTO AL HACER CLICK
+})
+
+
 
 /*INSTRUCCIONES;
 Crea dos funciones, add y multiply. 
